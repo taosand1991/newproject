b82d9327839d301bfee7760cc9cad595195bc948
TAOSAND <taofeekdboy@yahoo.com>
2019-12-15 15:39:49 +0400

    my project

---
+++ accounts/middleware.py
@@ -0,0 +1,17 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponseForbidden, Http404, H
+from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import reverse
+
+
+class RestrictStaffToAdminMiddleware(MiddlewareMixin):
+    """
+    A middleware that restricts staff members access to administration panels.
+    """
+    def process_request(self, request):
+        if request.path.startswith(reverse('admin:index')):
+            if request.user.is_authenticated:
+                if not request.user.is_staff:
+                    raise Http404
+            else:
+                raise Http404
--- website/settings.py
+++ website/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'accounts.middleware.RestrictStaffToAdminMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
